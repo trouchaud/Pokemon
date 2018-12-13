2e72665ea9387030c8248156f24457acd1eddda2
Alexandre <trouchaud.alexandre@outlook.fr>
2018-12-13 02:18:07 +0100

    ADD: arena + FIX: modification de la structure des vues

--- 05-gui/src/main/java/com/ifi/tp/controller/TrainerController.java
+++ 05-gui/src/main/java/com/ifi/tp/controller/TrainerController.java
@@ -1,5 +1,6 @@
 package com.ifi.tp.controller;
 
+import com.ifi.tp.trainers.bo.Trainer;
 import com.ifi.tp.trainers.service.TrainerService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,25 +8,57 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @Controller
 public class TrainerController {
 
+    final String viewDirectory = "trainer/";
+
     @Autowired
     private TrainerService trainerService;
 
+    @GetMapping("/arena/{name}")
+    String getTrainers(ModelMap model, @PathVariable String name){
+        model.addAttribute("trainer", trainerService.getTrainer(name));
+        return viewDirectory.concat("arenaFight");
+    }
+
     @GetMapping("/trainers")
-    String index(ModelMap model){
+    String getTrainers(ModelMap model){
         model.addAttribute("trainers", trainerService.getAllTrainers());
-        return "trainers";
+        return viewDirectory.concat("trainers");
     }
 
     @GetMapping("/trainers/{name}")
-    String index(ModelMap model, @PathVariable String name){
+    String getTrainer(ModelMap model, @PathVariable String name){
         model.addAttribute("trainer", trainerService.getTrainer(name));
+        return viewDirectory.concat("trainer");
+    }
 
-        return "trainer";
+    @GetMapping("/team/{name}")
+    String getTeam(ModelMap model, @PathVariable String name){
+        model.addAttribute("trainer", trainerService.getTrainer(name));
+        return viewDirectory.concat("team");
+    }
+
+    @GetMapping("/trainers/{name}/arena")
+    String getOtherTrainer(ModelMap model, @PathVariable String name){
+        List<Trainer> oldTrainers = trainerService.getAllTrainers();
+        List<Trainer> trainers = new ArrayList<Trainer>();
+
+        for (Iterator<Trainer> iter = oldTrainers.listIterator(); iter.hasNext(); ) {
+            Trainer a = iter.next();
+            if (!a.getName().equals(name)) {
+                trainers.add(a);
+            }
+        }
+
+        model.addAttribute("trainers", trainers);
+        return viewDirectory.concat("fightTrainer");
     }
 
 }
